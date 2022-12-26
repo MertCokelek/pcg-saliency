c899b71f172a72cec5fb4e3022c396b9d8c5f1ca
MertCokelek <mcokelek21@ku.edu.tr>
2022-12-26 15:56:46 +0300

    Path changes

--- main_pcg.m
+++ main_pcg.m
@@ -3,7 +3,7 @@ close all;
 clc;
 
 prompt = 'Enter .wav filename (without .extension):'; 
-%% example: "/home/mertcokelek/Desktop/Dataset/2530_TV";
+%% example: "/home/mertcokelek/Desktop/PCG-Data/2530_TV";
 audioname = input(prompt, 's');
 wav_path = strcat(audioname, ".wav");
 output_path = strcat(audioname, "_saliency.mat");
@@ -15,7 +15,7 @@ infoaudio = audioinfo(wav_path);
 sdata = audioread(wav_path);
 t = linspace(0, infoaudio.Duration, length(sdata));
 Fs = infoaudio.SampleRate;
-audio_in_seconds = zeros(int16(infoaudio.Duration), Fs, 1);
+saliency = zeros(int16(infoaudio.Duration), Fs, 1);
 disp(Fs);
 
 %% I guess it's processing 1-sec clips. (Short-time).
@@ -29,7 +29,7 @@ for i = 1:infoaudio.Duration;
     a = 1;
 
     hW = filter(b, a, sW);
-    audio_in_seconds(i, :, 1) = hW;
+    saliency(i, :, 1) = hW;
 end
 
 save(output_path, 'saliency', '-v6');
