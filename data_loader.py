a6bb80c4a51d26b7916c8b94624c4442c6ae8fd3
tanjary21 <tanjary21@ku.edu.tr>
2022-12-31 12:52:30 +0300

    modified data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -11,6 +11,9 @@ from sklearn.model_selection import train_test_split
 import librosa
 import librosa.display
 
+import scipy.io
+from torch.utils.data import Dataset
+
 class Data_loader():
     def __init__(self, path_demo, path_sig, encode=True):
         # self.subject_id = subject_id
@@ -45,3 +48,45 @@ class Data_loader():
 # data_sample = Data_loader(path_demo, path_sig)
 # data = data_sample.get_item(0,'TV')
 # print(data)
+
+class Saliency_loader(Dataset):
+    def __init__(self, path_demo, path_sal, encode=True):
+        # self.subject_id = subject_id
+        self.path_sal = path_sal
+        self.demo_data = pd.read_csv(path_demo) # example:~/courses/bio-sig/datasets/D1/physionet.org/files/circor-heart-sound/1.0.3/training_data.csv
+       
+       
+        self.subject_ids = self.demo_data[self.demo_data['Recording locations:'] == "AV+PV+TV+MV"]['Patient ID'] # getting all the subjects that all the recordings are available
+        self.demo_data = self.demo_data[["Patient ID", "Murmur", "Outcome"]]
+        if encode:
+            label_encoder= LabelEncoder()
+            self.demo_data['Murmur']= label_encoder.fit_transform(self.demo_data['Murmur']) # Murmur is absent/present/Unknown 
+            self.demo_data['Outcome']= label_encoder.fit_transform(self.demo_data['Outcome']) # Diagnosed by the medical expert: normal/abnormal
+ 
+    
+    def __len__(self):
+        return self.subject_ids.shape[0]
+
+    
+    def __getitem__(self, index):
+        ret = {}
+        ret['subject_id'] = self.subject_ids[index]
+        
+        ret['saliencies'] = {}
+        path_root = f"{self.path_sal}/{self.subject_ids[index]}"
+        subject_AV, subject_TV, subject_MV, subject_PV = f"{path_root}_AV_saliency.mat", f"{path_root}_TV_saliency.mat", f"{path_root}_MV_saliency.mat", f"{path_root}_PV_saliency.mat"
+        subject_AV, subject_TV, subject_MV, subject_PV = scipy.io.loadmat(subject_AV), scipy.io.loadmat(subject_TV), scipy.io.loadmat(subject_MV), scipy.io.loadmat(subject_PV)
+        ret['saliencies']['AV'], ret['saliencies']['TV'], ret['saliencies']['MV'], ret['saliencies']['PV'] = subject_AV['saliency'], subject_TV['saliency'], subject_MV['saliency'], subject_PV['saliency']
+        
+        ret['murmur'], ret['outcome'] = self.demo_data[self.demo_data['Patient ID']==self.subject_ids[index]][['Murmur', 'Outcome']].iloc[0]
+        return ret
+
+# #instance
+# from torch.utils.data import DataLoader
+# path_demo = "../data/moodyData/physionet.org/files/circor-heart-sound/1.0.3/training_data.csv"
+# path_sig = "../data/moodyData/physionet.org/files/circor-heart-sound/1.0.3/training_data"
+# path_sal = "../output"
+# saliency_dataset = Saliency_loader(path_demo, path_sal)
+# saliency_loader = DataLoader(saliency_dataset)
+# data = next(iter(saliency_loader))
+# print(data['saliency']['AV'].shape)
