da355c5d90f349b94d3a008416b2037d7646625f
tanjary21 <tanjary21@ku.edu.tr>
2023-01-10 12:31:27 +0300

    Updated train script Now we have train val splits and separate train_epoch() and  val_epoch() functions

--- train_energy_classifier.py
+++ train_energy_classifier.py
@@ -19,6 +19,50 @@ import wandb
 
 import argparse
 
+def train_epoch(train_loader, energy_classifier, optimizer, weights, args):
+    energy_classifier.train()
+    for subject_data in train_loader:
+        #subject_AV_sal, subject_TV_sal, subject_PV_sal, subject_MV_sal, label = subject_data['saliencies']['AV'].squeeze(0), subject_data['saliencies']['TV'].squeeze(0), subject_data['saliencies']['PV'].squeeze(0), subject_data['saliencies']['MV'].squeeze(0), subject_data['murmur']
+        energies, labels = subject_data
+        
+        optimizer.zero_grad()
+
+        #logits = energy_classifier([subject_AV_sal.float(), subject_TV_sal.float(), subject_PV_sal.float(), subject_MV_sal.float()])
+        logits = energy_classifier(energies)
+        
+        loss = F.nll_loss(logits.softmax(1).log(), labels, weight=weights) #weights[labels] * F.mse_loss(logits, F.one_hot(labels, num_classes=3).float().squeeze(0)) #F.nll_loss(logits.unsqueeze(0), label)
+
+        loss.backward()
+
+        optimizer.step()
+
+        probs = logits.softmax(1) #F.softmax(logits, dim=0).unsqueeze(0)
+
+        #probs_epoch.append(probs), labels_epoch.append(label)
+        if args.log:
+            acc, prec, rec = accuracy(probs, labels).item(), precision(probs, labels).item(), recall(probs, labels).item()
+            wandb.log({'loss': loss.item(), 'accuracy': acc, 'precision': prec, 'recall': rec})
+
+    return energy_classifier
+
+def val_epoch(val_loader, energy_classifier, weights, args):
+    energy_classifier.eval()
+    with torch.no_grad():
+        for subject_data in val_loader:
+            #subject_AV_sal, subject_TV_sal, subject_PV_sal, subject_MV_sal, label = subject_data['saliencies']['AV'].squeeze(0), subject_data['saliencies']['TV'].squeeze(0), subject_data['saliencies']['PV'].squeeze(0), subject_data['saliencies']['MV'].squeeze(0), subject_data['murmur']
+            energies, labels = subject_data
+            
+            logits = energy_classifier(energies)
+            
+            loss = F.nll_loss(logits.softmax(1).log(), labels, weight=weights) #weights[labels] * F.mse_loss(logits, F.one_hot(labels, num_classes=3).float().squeeze(0)) #F.nll_loss(logits.unsqueeze(0), label)
+            
+            probs = logits.softmax(1) #F.softmax(logits, dim=0).unsqueeze(0)
+
+            #probs_epoch.append(probs), labels_epoch.append(label)
+            if args.log:
+                acc, prec, rec = accuracy(probs, labels).item(), precision(probs, labels).item(), recall(probs, labels).item()
+                wandb.log({'val loss': loss.item(), 'val accuracy': acc, 'val precision': prec, 'val recall': rec})
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--log', action="store_true")
@@ -32,7 +76,11 @@ if __name__ == '__main__':
     path_sig = "../data/moodyData/physionet.org/files/circor-heart-sound/1.0.3/training_data"
     path_sal = "../output"
     saliency_dataset = Saliency_loader(path_demo, path_sal, energy_loader=True)
-    saliency_loader = DataLoader(saliency_dataset, shuffle=True, batch_size=20)
+    train_size = int(0.7 * len(saliency_dataset))
+    val_size = len(saliency_dataset) - train_size
+    train_dataset, val_dataset = torch.utils.data.random_split(saliency_dataset, [train_size, val_size])
+    train_loader, val_loader = DataLoader(train_dataset, shuffle=True, batch_size=20), DataLoader(val_dataset, shuffle=True, batch_size=len(val_dataset))
+
     # data = next(iter(saliency_loader))
     # print(data['saliency']['AV'].shape)
 
@@ -46,29 +94,10 @@ if __name__ == '__main__':
     weights = torch.Tensor([1, 3.94, 35.0]) # weights for balancing classes [0,1,2] respectively
 
     for epoch in range(100):
-        probs_epoch, labels_epoch = [], []
-
-        for subject_data in saliency_loader:
-            #subject_AV_sal, subject_TV_sal, subject_PV_sal, subject_MV_sal, label = subject_data['saliencies']['AV'].squeeze(0), subject_data['saliencies']['TV'].squeeze(0), subject_data['saliencies']['PV'].squeeze(0), subject_data['saliencies']['MV'].squeeze(0), subject_data['murmur']
-            energies, labels = subject_data
-            
-            optimizer.zero_grad()
-
-            #logits = energy_classifier([subject_AV_sal.float(), subject_TV_sal.float(), subject_PV_sal.float(), subject_MV_sal.float()])
-            logits = energy_classifier(energies)
-            
-            loss = F.nll_loss(logits.softmax(1).log(), labels, weight=weights) #weights[labels] * F.mse_loss(logits, F.one_hot(labels, num_classes=3).float().squeeze(0)) #F.nll_loss(logits.unsqueeze(0), label)
-
-            loss.backward()
-
-            optimizer.step()
-
-            probs = logits.softmax(1) #F.softmax(logits, dim=0).unsqueeze(0)
-
-            #probs_epoch.append(probs), labels_epoch.append(label)
-            if args.log:
-                acc, prec, rec = accuracy(probs, labels).item(), precision(probs, labels).item(), recall(probs, labels).item()
-                wandb.log({'loss': loss.item(), 'accuracy': acc, 'precision': prec, 'recall': rec})
+       
+       energy_classifier = train_epoch(train_loader, energy_classifier, optimizer, weights, args)
+       val_epoch(val_loader, energy_classifier, weights, args)
+        
         
         #probs_epoch, labels_epoch = torch.concat(probs_epoch), torch.concat(labels_epoch)
         #acc, prec, rec = accuracy(probs_epoch, labels_epoch).item(), precision(probs_epoch, labels_epoch).item(), recall(probs_epoch, labels_epoch).item()
