db4d218c342401b9e970e40cc320af10ce2624e2
ajamshidi18 <ajamshidi18@ai03.kuacc.ku.edu.tr>
2023-01-15 10:58:43 +0300

    update train and val

--- train_conv1d.py
+++ train_conv1d.py
@@ -27,17 +27,17 @@ path_sig = "../../datasets/D1/physionet.org/files/circor-heart-sound/1.0.3/train
 path_sal = "/userfiles/tanjary21/saliencies/output"
 
 saliency_dataset = Saliency_loader(path_demo, path_sal)
-saliency_loader = DataLoader(saliency_dataset,batch_size = 1, shuffle=True )
-# train_set, test_set = torch.utils.data.random_split(saliency_dataset, [int(0.8*len(saliency_dataset)), int(0.2 * len(saliency_dataset))])
-# train_data_loader = DataLoader(dataset= train_set, batch_size = 4, shuffle= True, num_workers=2)
-# test_data_loader = DataLoader(dataset =test_set, batch_size = 4, shuffle = True , num_workers=2 )
+# saliency_loader = DataLoader(saliency_dataset,batch_size = 1, shuffle=True ) # to train only with the whole available data
+train_set, test_set = torch.utils.data.random_split(saliency_dataset, [int(0.7*len(saliency_dataset))+1, int(0.3 * len(saliency_dataset))])
+train_data_loader = DataLoader(dataset= train_set, batch_size = 1, shuffle= True ) #num_workers=2
+test_data_loader = DataLoader(dataset =test_set, batch_size = 1, shuffle = True )#, num_workers=2 
 
 wandb.init(project="Elec547")
 # define the model hyperparameters
 in_channels=1
 n_depth = 1
 output_size=5
-kernel_size = 3
+kernel_size = 5
 n_classes=3
 # create the model
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -52,12 +52,13 @@ recall = Recall(task='multiclass', num_classes=3)
 # train the model
 for epochs in range(n_epochs):
     predlabels_epoch, labels_epoch = [], []
-    for subject_data in saliency_loader:
+    for subject_data in train_data_loader: # saliency_loader
         label = subject_data['murmur']
         # subject_AV_sal, subject_TV_sal, subject_PV_sal, subject_MV_sal, labels = subject_data['saliencies']['AV'].squeeze(0), subject_data['saliencies']['TV'].squeeze(0), subject_data['saliencies']['PV'].squeeze(0), subject_data['saliencies']['MV'].squeeze(0), subject_data['murmur']
         labels_epoch.append(label)
         # forward pass
         y_pred = model(subject_data) # the 4 signals are fed to the model, check the classifiers.py\Conv1D_classifier 
+        # print(y_pred)
         loss = criterion(y_pred, label)
         predlabels_epoch.append(y_pred)
         # backward pass and optimize
@@ -67,4 +68,22 @@ for epochs in range(n_epochs):
     predlabels_epoch, labels_epoch = torch.concat(predlabels_epoch), torch.concat(labels_epoch)
     acc, prec, rec = accuracy(predlabels_epoch, labels_epoch).item(), precision(predlabels_epoch, labels_epoch).item(), recall(predlabels_epoch, labels_epoch).item()
 
-    wandb.log({'accuracy': acc, 'precision': prec, 'recall': rec, 'loss': loss.item()})
+    wandb.log({'train_accuracy': acc, 'train_precision': prec, 'train_recall': rec, 'train_loss': loss.item()})
+
+# validating the model  
+model.eval()
+gt_labels=[]
+pred_labels=[]
+with torch.no_grad():
+        for subject_data in test_data_loader:
+            gt_label= subject_data["murmur"]
+            gt_labels.append(gt_label)
+            pred_label = model(subject_data)
+            # print(pred_label)
+            pred_labels.append(pred_label)
+            
+        # print(pred_labels)
+        pred_labels, gt_labels = torch.tensor(torch.concat(pred_labels)), torch.tensor(torch.concat(gt_labels))
+        # pred_labels, gt_labels = torch.tensor(pred_labels), torch.tensor(gt_labels)
+        acc, prec, rec = accuracy(pred_labels, gt_labels), precision(pred_labels, gt_labels), recall(pred_labels, gt_labels)
+        wandb.log({'val accuracy': acc, 'val precision': prec, 'val recall': rec})
